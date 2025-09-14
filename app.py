933cac06d68f540c78b3bd210620ec40e17ebee3
Krishsharma179 <krishsharma5272@gmail.com>
2025-09-14 16:03:04 +0530

    Today I am working on ANN diabetes classification model

---
+++ app.py
@@ -0,0 +1,54 @@
+from tensorflow.keras.models import load_model
+import pickle
+import pandas as pd
+import numpy as np
+import streamlit as st
+
+model=load_model('model.h5')
+
+with open('preprocessor.pkl','rb') as file:
+    preprocessor=pickle.load(file)
+
+st.title('diabetes_prediction')
+
+
+Pregnancies=st.number_input('Pregnancies')
+Glucose=st.number_input('Gluscose')
+BloodPressure=st.number_input('BloodPressure') 
+SkinThickness=st.number_input('SkinThickness') 
+Insulin=st.number_input('Insulin') 
+BMI=st.number_input('BMI') 
+DiabetesPedigreeFunction=st.number_input('DiabetesPedigreeFunction') 
+Age=st.number_input('Age')
+
+user_input={
+    "Pregnancies": [Pregnancies],
+    "Glucose": [Glucose],
+    "BloodPressure": [BloodPressure],
+    "SkinThickness": [SkinThickness],
+    "Insulin": [Insulin],
+    "BMI": [BMI],
+    "DiabetesPedigreeFunction": [DiabetesPedigreeFunction],
+    "Age": [Age]
+}
+
+df=pd.DataFrame(user_input)
+
+df=preprocessor.transform(df)
+
+encoded_df=pd.DataFrame(df)
+
+pred=model.predict(encoded_df)
+
+prob=pred[0][0]
+
+if prob>0.5:
+     st.write("Diabetes found")
+else:
+     st.write("Diabetes not found")    
+
+
+
+
+
+
